fefc63d57aa9a04c26af283ae7f843b77ebf1e30
Valentin Morev <valentin.morev@live.com>
2011-11-20 15:44:45 +0300

    Logging added

--- src/com/vmorev/gdoc2ts/DocumentLoader.java
+++ src/com/vmorev/gdoc2ts/DocumentLoader.java
@@ -4,6 +4,8 @@ import com.google.gdata.util.ServiceException;
 import com.vmorev.gdoc2ts.connector.GDocConnector;
 import com.vmorev.gdoc2ts.connector.TSConnector;
 import com.vmorev.gdoc2ts.model.AbstractDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import terrastore.client.BucketOperation;
 import terrastore.client.Values;
 
@@ -17,6 +19,7 @@ import java.util.Map;
  * Date: 11/13/11 3:00 PM
  */
 public class DocumentLoader {
+    private final Logger logger = LoggerFactory.getLogger(DocumentLoader.class);
     private TSConnector tsConnector;
     private GDocConnector gDocConnector;
 
@@ -32,6 +35,7 @@ public class DocumentLoader {
         Map<String, AbstractDocument> toLoad = new HashMap<String, AbstractDocument>(gDocs.size());
 
         for (AbstractDocument entry : gDocs) {
+            logger.info("GDoc entry found with ID: " + entry.getResourceId() + " and title: " + entry.getTitle());
             if (AbstractDocument.DOC_TYPE_FOLDER.equals(entry.getType())) {
                 syncFolder(entry.getResourceId());
             } else if (AbstractDocument.DOC_TYPE_DOCUMENT.equals(entry.getType())) {
